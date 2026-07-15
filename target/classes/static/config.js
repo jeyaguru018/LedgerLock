969dc26a735cffedc37b9bcb6c311ad7b8fa6884
Jeyaguru M K <jeyaguru018@gmail.com>
2026-07-15 09:01:56 +0530

    chore: add .gitignore and remove target directory

--- target/classes/static/config.js
+++
@@ -1,6 +0,0 @@
-// config.js
-// This file is meant to be overridden during deployment (e.g., Vercel/Netlify builds)
-// to point the frontend to the correct backend API URL.
-window.CONFIG = {
-    API_BASE_URL: "" // e.g., "https://ledgerlock-api.onrender.com"
-};
